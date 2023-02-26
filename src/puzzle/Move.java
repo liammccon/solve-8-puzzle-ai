150b6b03932fe93c9e7dcd7154eeee77595fc71c
liammccon <liam.t.mccon@gmail.com>
2023-02-26 14:52:11 -0500

    Create record for Move

---
+++ src/puzzle/Move.java
@@ -0,0 +1,17 @@
+package puzzle;
+
+/**
+ * Represents a move of the blank tile in one direction, resulting in the new board state.
+ * @param direction the direction in which to move the blank tile. Will be null for the initial move.
+ * @param prev the previous move in the current sequence, going back to the start. Will be null for the initial move.
+ * @param distFromStart represents g(n), the number of moves taken since the first move in this sequence. Starts as 0
+ * @param heuristic represents h(n), the heuristic
+ * @param newState the state of the board after this move
+ */
+public record Move(Direction direction, Move prev, int distFromStart, int heuristic, String newState) {
+    /**
+     * Returns the cost, f(n) = g(n) + h(n)
+     * @return the cost, f(n) = g(n) + h(n)
+     */
+    public int cost() { return distFromStart + heuristic;}
+}
